7d41a75a447950488689a0fb5f640fa8be247af0
H4LL <adam.hall@napier.ac.uk>
2020-02-17 14:47:34 +0000

    update for running on an ubuntu vm

--- installation.sh
+++ installation.sh
@@ -17,6 +17,7 @@ cd /tmp
 curl -O https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
 bash Anaconda3-2019.03-Linux-x86_64.sh
 source ~/.bashrc
+export PATH=~/anaconda3/bin:$PATH
 
 #Install PySyft req
 conda create -n pysyft python=3.7
