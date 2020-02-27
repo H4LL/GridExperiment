82a19e5367665707a6d425527f9f26b0855b3dde
H4LL <adam.hall@napier.ac.uk>
2020-02-27 19:02:50 +0100

    added conda fix

--- installation.sh
+++ installation.sh
@@ -16,6 +16,7 @@ sudo apt install docker-compose
 cd /tmp
 curl -O https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
 bash Anaconda3-2019.03-Linux-x86_64.sh
+export PATH=~/anaconda3/bin:$PATH
 source ~/.bashrc
 
 #Install PySyft req
