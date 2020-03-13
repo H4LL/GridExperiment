ce1b14163ed8b9488f6e09aca8fc3a232e2115d4
H4LL <adam.hall@napier.ac.uk>
2020-03-12 21:17:47 -0700

    fix path names

--- python/plot-netO.py
+++ python/plot-netO.py
@@ -3,27 +3,27 @@ import matplotlib.pyplot as plt
 
 
 # load cpu data
-pygrid_james_1 = pd.read_csv('results/graphs/pygrid_james_1/networkoutput.csv', delimiter=' ', names=['val'])
-pygrid_bill_1 = pd.read_csv('results/graphs/pygrid_bill_1/networkoutput.csv', delimiter=' ', names=['val'])
-pygrid_alice_1 = pd.read_csv('results/graphs/pygrid_alice_1/networkoutput.csv', delimiter=' ', names=['val'])
-pygrid_bob_1 = pd.read_csv('results/graphs/pygrid_bob_1/networkoutput.csv', delimiter=' ', names=['val'])
-pygrid_gateway_1 = pd.read_csv('results/graphs/pygrid_gateway_1/networkoutput.csv', delimiter=' ', names=['val'])
+gridexperiment_james_1 = pd.read_csv('results/graphs/gridexperiment_james_1/networkoutput.csv', delimiter=' ', names=['val'])
+gridexperiment_bill_1 = pd.read_csv('results/graphs/gridexperiment_bill_1/networkoutput.csv', delimiter=' ', names=['val'])
+gridexperiment_alice_1 = pd.read_csv('results/graphs/gridexperiment_alice_1/networkoutput.csv', delimiter=' ', names=['val'])
+gridexperiment_bob_1 = pd.read_csv('results/graphs/gridexperiment_bob_1/networkoutput.csv', delimiter=' ', names=['val'])
+gridexperiment_gateway_1 = pd.read_csv('results/graphs/gridexperiment_gateway_1/networkoutput.csv', delimiter=' ', names=['val'])
 
 # Create seconds labels
 x = []
 i = 1
-while (i < (len(pygrid_james_1)+1)):
+while (i < (len(gridexperiment_james_1)+1)):
     x.append(i*3)
     i+=1
 
 # plot
-plt.plot(x, pygrid_james_1['val'], label='pygrid_james_1')
-plt.plot(x, pygrid_bill_1['val'], label='pygrid_bill_1')
-plt.plot(x, pygrid_alice_1['val'], label='pygrid_alice_1')
-plt.plot(x, pygrid_bob_1['val'], label='pygrid_bob_1')
-plt.plot(x, pygrid_gateway_1['val'], label='pygrid_gateway_1')
+plt.plot(x, gridexperiment_james_1['val'], label='gridexperiment_james_1')
+plt.plot(x, gridexperiment_bill_1['val'], label='gridexperiment_bill_1')
+plt.plot(x, gridexperiment_alice_1['val'], label='gridexperiment_alice_1')
+plt.plot(x, gridexperiment_bob_1['val'], label='gridexperiment_bob_1')
+plt.plot(x, gridexperiment_gateway_1['val'], label='gridexperiment_gateway_1')
 plt.legend()
 plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 plt.xlabel("Time (seconds)")
 plt.ylabel("CPU Usage (%)")
-plt.savefig('results/graphs/BlockO_usage.png', dpi=800, bbox_inches='tight')
+plt.savefig('results/graphs/NetO_usage.png', dpi=800, bbox_inches='tight')
