b6a592aba991f455fdf50c50786ebb99c7c8ef2e
H4LL <adam.hall@napier.ac.uk>
2020-03-13 00:22:44 -0700

    MB to KB done

--- python/plot-netI.py
+++ python/plot-netI.py
@@ -16,6 +16,24 @@ while (i < (len(gridexperiment_james_1)+1)):
     x.append(i*3)
     i+=1
 
+def convkb(text):
+    try:
+        if text[-2:]=='kB':
+            return text[:-2]
+        elif text[-2:]=='MB':
+            return float(text[:-2])*1024
+        elif text[-1:]=='B':
+            return float((text[:-1]))/1024
+    except AttributeError:
+        return text
+
+# Convert to KB
+gridexperiment_james_1['val'] = gridexperiment_james_1['val'].apply(convkb)
+gridexperiment_bill_1['val'] = gridexperiment_bill_1['val'].apply(convkb)
+gridexperiment_alice_1['val'] = gridexperiment_alice_1['val'].apply(convkb)
+gridexperiment_bob_1['val'] = gridexperiment_bob_1['val'].apply(convkb)
+gridexperiment_gateway_1['val'] = gridexperiment_gateway_1['val'].apply(convkb)
+
 # plot
 plt.plot(x, gridexperiment_james_1['val'], label='gridexperiment_james_1')
 plt.plot(x, gridexperiment_bill_1['val'], label='gridexperiment_bill_1')
